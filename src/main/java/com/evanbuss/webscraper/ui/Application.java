dfa09edd0ddf51dfb61332083b734d2edb79b352
Evan Buss <evan.buss28@gmail.com>
2019-10-10 00:26:21 -0400

    Added basic logging support - Decided to go with SLF4J and Logback as they seem to be industry standard - Working on learning and getting my own log configuration set up

--- src/main/java/com/evanbuss/webscraper/ui/Application.java
+++ src/main/java/com/evanbuss/webscraper/ui/Application.java
@@ -4,11 +4,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application extends javafx.application.Application {
 
+  static final Logger logger = LoggerFactory.getLogger(Application.class);
+
   @Override
   public void start(Stage primaryStage) throws Exception {
+    logger.info("Starting application");
+    logger.debug("test");
     Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
     Scene scene = new Scene(root);
     scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
