994c2e6ab40eeae465957220d779a64eeba4c258
Evan Buss <evan.buss28@gmail.com>
2019-10-10 15:13:11 -0400

    logging tweaks

--- src/main/java/com/evanbuss/webscraper/ui/Application.java
+++ src/main/java/com/evanbuss/webscraper/ui/Application.java
@@ -9,12 +9,11 @@ import org.slf4j.LoggerFactory;
 
 public class Application extends javafx.application.Application {
 
-  static final Logger logger = LoggerFactory.getLogger(Application.class);
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   @Override
   public void start(Stage primaryStage) throws Exception {
     logger.info("Starting application");
-    logger.debug("test");
     Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
     Scene scene = new Scene(root);
     scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
